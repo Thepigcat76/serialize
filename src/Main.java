cb0e98af1fdadb283f68b670b1dbdc806d8571a2
thepigcat <benpospo@gmail.com>
2024-06-26 13:59:45 +0200

    refactor methods

--- src/Main.java
+++ src/Main.java
@@ -36,32 +36,47 @@ public class Main {
         Field[] fields = instance.getClass().getDeclaredFields();
         for (Field field : fields) {
             if (field.isAnnotationPresent(Serialized.class)) {
-                Serialized serialized = field.getAnnotation(Serialized.class);
-                Class<? extends Serializer<?>> serializerClass = switch (serialized.value().length) {
-                    case 0 -> {
-                        Class<? extends Serializer<?>> serializer = DEFAULT_SERIALIZER.get(field.getType());
-                        if (serializer != null)
-                            yield serializer;
-                        else
-                            throw new Exception("Could not get default serializer for type: " + field.getType().getTypeName() + ". Please provide a serializer manually by passing it to the annotation");
-                    }
-                    case 1 -> serialized.value()[0];
-                    default -> throw new Error("Provided more than one serializer for field " + field);
-                };
-                Method instanceMethod = getInstanceMethod(serializerClass);
-                Serializer<?> serializer = null;
-                try {
-                    Object result = instanceMethod.invoke(null);
-                    if (result instanceof Serializer<?> serializer1) {
-                        serializer = serializer1;
-                    }
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                    throw new RuntimeException(e);
-                }
-                int i = field.getInt(instance);
-                DataManager.INSTANCE.save(field.getName(), i);
+                serializeField(instance, field);
             }
         }
+    }
+
+    private static void serializeField(Test instance, Field field) throws Exception {
+        Serialized serialized = field.getAnnotation(Serialized.class);
+        Class<? extends Serializer<?>> serializerClass = switch (serialized.value().length) {
+            case 0 -> findDefaultSerializer(field);
+            case 1 -> serialized.value()[0];
+            default -> throw new Error("Provided more than one serializer for field " + field);
+        };
+        Method instanceMethod = getInstanceMethod(serializerClass);
+        Serializer<?> serializer = getCustomSerializer(instanceMethod);
+        int i = field.getInt(instance);
+        DataManager.INSTANCE.save(field.getName(), i);
+    }
+
+    private static Serializer<?> getCustomSerializer(Method instanceMethod) {
+        Serializer<?> serializer = null;
+        try {
+            Object result = instanceMethod.invoke(null);
+            if (result instanceof Serializer<?> serializer1) {
+                serializer = serializer1;
+            }
+            return serializer;
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Class<? extends Serializer<?>> findDefaultSerializer(Field field) throws Exception {
+        Class<? extends Serializer<?>> serializer = DEFAULT_SERIALIZER.get(field.getType());
+        if (serializer != null)
+            return serializer;
+        else
+            throw noDefaultSerializer(field);
+    }
+
+    private static Exception noDefaultSerializer(Field field) {
+        return new Exception("Could not get default serializer for type: " + field.getType().getTypeName() + ". Please provide a serializer manually by passing it to the annotation");
     }
 
     public static void load(Test instance) throws IllegalAccessException {
@@ -84,37 +99,53 @@ public class Main {
         for (Method method : methods) {
             if (method.isAnnotationPresent(SerializerInstance.class)) {
                 if (!Modifier.isStatic(method.getModifiers())) {
-                    throw new IllegalAccessException("Method `" + method.getName() + "` needs to be static in order to be a valid instance getter method");
+                    throw instanceMethodNotStatic(method);
                 }
                 if (method.getReturnType() != serializerClass) {
-                    throw new Exception("Return type of method `" + method.getName()
-                            + "` is not correct. Since this method is annotated as an instance getter method it needs to return the type of the instance and therefore it's class. Provided return type: "
-                            + method.getReturnType().getTypeName()
-                            + ", expected return type: "
-                            + serializerClass.getTypeName()
-                    );
+                    throw incorrectReturnTypeException(serializerClass, method);
                 }
                 if (instanceMethod == null) {
                     instanceMethod = method;
                 } else {
-                    throw new RuntimeException("Only one method in the Serializer class is allowed to have a @SerializerInstance annotation. Affected class: `"
-                            + serializerClass.getName()
-                            + "`, affected method: `"
-                            + method.getName()
-                            + "`, already present instance getter method: `"
-                            + instanceMethod.getName()
-                            + "`"
-                    );
+                    throw multipleInstancesError(serializerClass, method, instanceMethod);
                 }
             }
         }
 
         if (instanceMethod == null) {
-            throw new RuntimeException("Class: `"
-                    + serializerClass.getName()
-                    + "` does not have an instance getter method. Your serializer class needs a public, static method that will return a singleton instance of your Serializer. This method also needs to be annotated with `@SerializerInstance` and have the class as its return type");
+            throw noInstanceMethod(serializerClass);
         }
 
         return instanceMethod;
+    }
+
+    private static RuntimeException noInstanceMethod(Class<? extends Serializer<?>> serializerClass) {
+        return new RuntimeException("Class: `"
+                + serializerClass.getName()
+                + "` does not have an instance getter method. Your serializer class needs a public, static method that will return a singleton instance of your Serializer. This method also needs to be annotated with `@SerializerInstance` and have the class as its return type");
+    }
+
+    private static IllegalAccessException instanceMethodNotStatic(Method method) {
+        return new IllegalAccessException("Method `" + method.getName() + "` needs to be static in order to be a valid instance getter method");
+    }
+
+    private static Exception incorrectReturnTypeException(Class<? extends Serializer<?>> serializerClass, Method method) {
+        return new Exception("Return type of method `" + method.getName()
+                + "` is not correct. Since this method is annotated as an instance getter method it needs to return the type of the instance and therefore it's class. Provided return type: "
+                + method.getReturnType().getTypeName()
+                + ", expected return type: "
+                + serializerClass.getTypeName()
+        );
+    }
+
+    private static RuntimeException multipleInstancesError(Class<? extends Serializer<?>> serializerClass, Method method, Method instanceMethod) {
+        return new RuntimeException("Only one method in the Serializer class is allowed to have a @SerializerInstance annotation. Affected class: `"
+                + serializerClass.getName()
+                + "`, affected method: `"
+                + method.getName()
+                + "`, already present instance getter method: `"
+                + instanceMethod.getName()
+                + "`"
+        );
     }
 }
